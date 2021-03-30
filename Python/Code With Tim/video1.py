710cb4d4b9c0b694e29fcc33031ba2b625a36835
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-03-29 20:00:10 -0500

    🔥Little changes added

--- Python/Code With Tim/video1.py
+++ Python/Code With Tim/video1.py
@@ -1,7 +1,9 @@
 import cv2
 
 img = cv2.imread('C:\\Users\\juanm\\Documents\\GitHub\\Youngermaster\\OpenCV-Codes\\Python\\Code With Tim\\Assets\\cam.jpg', 1)
-img = cv2.resize(img, (400, 400))
+#img = cv2.resize(img, (400, 400))
+img = cv2.resize(img, (0, 0), fx=0.2, fy=0.2)
+#img = cv2.resize(img, cv2.cv2.ROTATE_90_CLOCKWISE)
 
 cv2.imshow('Image', img)
 cv2.waitKey(0)
