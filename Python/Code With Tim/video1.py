e52d72a46c4f34562a4657af8d89edd6ef1d3715
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-03-30 22:15:46 -0500

    🐍Video 1 finished

--- Python/Code With Tim/video1.py
+++ Python/Code With Tim/video1.py
@@ -2,9 +2,11 @@ import cv2
 
 img = cv2.imread('C:\\Users\\juanm\\Documents\\GitHub\\Youngermaster\\OpenCV-Codes\\Python\\Code With Tim\\Assets\\cam.jpg', 1)
 #img = cv2.resize(img, (400, 400))
-img = cv2.resize(img, (0, 0), fx=0.2, fy=0.2)
-#img = cv2.resize(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+img = cv2.resize(img, (0, 0), fx=0.2, fy=0.2) # Scales by a 0.2x factor.
+img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE) # Rotates.
 
-cv2.imshow('Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite('new_image_stored.jpg', img) # Stores the image previously modified (rotated in this context).
+
+cv2.imshow('Image', img) # Shows the image.
+cv2.waitKey(0) # Wait infinitely.
+cv2.destroyAllWindows() # At the end destroys all the windows generated.
